cc7b3930d8e0ec97689a07cabcd5572f36ecb828
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2023-03-12 21:55:07 -0500

    Se ajusta link de la alarma

---
+++ src/app/home/alarm-detail/alarm-detail.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { AlarmDetailComponent } from './alarm-detail.component';
+
+describe('AlarmDetailComponent', () => {
+  let component: AlarmDetailComponent;
+  let fixture: ComponentFixture<AlarmDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AlarmDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlarmDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
